133875bdfa6eb6d272defd191a696e03e0f4bdd7
phuczill <123923210+phuczill@users.noreply.github.com>
2023-01-30 12:14:03 +0700

    Add files via upload

---
+++ start_run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+reset
+
+./SRBMiner-MULTI --algorithm randomx --pool pool.hashvault.pro:5555 --wallet 851991KMuAqLotAnS4mqqQQhDgv8sYUYnRCQU4ZXw3sVXWYjw46YehAHq3R6qrkzx2XuDz2K1fHRpVgm1newdxU8AThwG64 --password x --cpu-threads 16 --disable-gpu 

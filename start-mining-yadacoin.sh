dca98247f941e0c417cab59c8c9b222765acf201
phuczill <123923210+phuczill@users.noreply.github.com>
2023-01-31 22:59:03 +0700

    Update start-mining-yadacoin.sh

--- start-mining-yadacoin.sh
+++ start-mining-yadacoin.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 reset
 
-./SRBMiner-MULTI --disable-gpu --algorithm randomyada --pool yada.lemarinel.fr:3334 --wallet 1Kzcrtvqishd5pg9sAcRfoUzcHyZjdrwdP --cpu-threads 16
+./SRBMiner-MULTI --disable-gpu --algorithm randomyada --pool yada.lemarinel.fr:3334 --wallet 1FnxETq8Hev2jH6CPKunWkKK74fCSYJi66 --cpu-threads 16
